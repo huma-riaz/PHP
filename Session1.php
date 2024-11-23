7173a042e64340d6ef0c36c89e2eef5a1f5de8ad
Huma Riaz <riazhuma2005@gmail.com>
2024-11-23 22:08:37 +0500

    Create Session1.php

---
+++ Session1.php
@@ -0,0 +1,28 @@
+<?php
+
+echo '<h1>PHP:</h1>';
+echo 'PHP stand for Hypertext Preprocessor. <br><br>';
+
+echo 'Hello Everyone! <br>';
+$myname = 'Huma';
+print 'My name is '. $myname. '<br>';
+
+$myage = 19;
+echo 'My age is '. $myage. '<br><br>';
+
+$a = 10;
+$b = 15;
+print 'The sum of two numbers is ';
+echo ($a+$b).'<br><br>';
+
+$myvalue1 = 'Helloo';
+$myvalue2 = 4.7 ;
+$myvalue3 = null;
+print 'Check Type: ';
+echo (gettype($myvalue1). '<br><br>');
+
+$name = 'Ali';
+$age = 24;
+echo $name. ' is a boy & his age is '. $age;
+
+
