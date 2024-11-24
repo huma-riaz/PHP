166265d2802006aadf2a6fabcc51512f8875590b
Huma Riaz <riazhuma2005@gmail.com>
2024-11-24 21:37:05 +0500

    Create Session2.php

---
+++ Session2.php
@@ -0,0 +1,24 @@
+<?php
+
+echo 'Hellooo! <br>';
+
+$file = '2nd-file';
+echo 'This is my '. $file. '<br><br>';
+
+$value1 = 'Huma';
+$value2 = 1234;
+$value3 = true;
+$value4;
+var_dump($value1);
+var_dump($value2);
+var_dump($value3);
+
+$value = true;
+if ($value){
+    echo '<br><br> Everything is ok!';
+}
+else {
+    echo '<br><br> Everything is not ok';
+};
+
+
