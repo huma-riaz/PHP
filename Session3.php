335771de960f0c8708cdb3237ab72f3ea45001ca
Huma Riaz <riazhuma2005@gmail.com>
2024-11-26 21:07:28 +0500

    Create Session3.php

---
+++ Session3.php
@@ -0,0 +1,36 @@
+<?php
+$day = 'Sunday';
+switch ($day){
+    case 'Sunday';
+    echo 'Today is Sunday';
+    break;
+
+    case 'Monday';
+    echo 'Today is Monday';
+    break;
+
+    case 'Tuesday';
+    echo 'Today is Tuesday';
+    break;
+
+    case 'Wednesday';
+    echo 'Today is Wednesday';
+    break;
+
+    case 'Thursday';
+    echo 'Today is Thursday';
+    break;
+
+    case 'Friday';
+    echo 'Today is Friday';
+    break;
+
+    case 'Saturday';
+    echo 'Today is Saturday';
+    break;
+
+    default;
+    echo 'Not matched.';
+}
+
+?>
