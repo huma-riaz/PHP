ecbd48d222e0cfc743d1628ae0c46b7cce2203e0
Huma Riaz <riazhuma2005@gmail.com>
2024-12-01 19:50:42 +0500

    Create table.php

---
+++ table.php
@@ -0,0 +1,23 @@
+<?php
+
+echo '<h1>TABLES: </h1>';
+echo 'Table Of 2: <br><br>';
+$table = 2 ;
+$end = 10;
+
+for( $n= 1; $n <= $end; $n++){
+    echo $table . ' x ', $n . ' = '. $n * $table. '<br>';
+}
+echo '-----------------<br><br>';
+
+echo 'Table Of 10: <br><br>';
+$tablee = 10;
+$ending = 10; 
+
+for ($n = 1; $n <= $ending; $n++){
+    echo $tablee . ' x '. $n . ' = '. $n * $tablee . '<br>';
+};
+
+echo '-----------------<br>';
+
+?>
