39025ff161f3b9c021449f5f2780594444e61a4a
Huma Riaz <riazhuma2005@gmail.com>
2024-12-05 22:02:28 +0500

    Update

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+
+print_r($_POST); 
+echo '<br><br><br><br>';
+
+$name = $_POST['name'];
+echo 'My name is ' . $name .'<br><br>';
+
+$email = $_POST['email'];
+echo 'This is my email:  ' . $email .'<br><br>';
+
+$gender = $_POST['gender'];
+echo 'Gender: ' . $gender . '<br><br>';
+
+$city = $_POST['city'];
+echo 'City Name: ' . $city . '<br><br>';
+
+$skills = $_POST['skills'];
+echo 'My interests are : <br>' . implode(',' , $_POST['skills']) . '<br><br>';
+
+$message = $_POST['message'];
+echo 'Message: ' . $message . '<br><br>';
+
+?>
