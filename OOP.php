2d3add112a113331a2ac3abc53bd520aae20dce8
Huma Riaz <riazhuma2005@gmail.com>
2025-01-03 00:05:38 +0500

    Update OOP.php

--- OOP.php
+++ OOP.php
@@ -1,7 +1,21 @@
 <?php
-class Students{
-    
+class employee{
+    public $name;
+    public $age;
+    public $salary;
+
+    function work() {
+        echo 'My name is ' .$this->name. '<br>'. 'I am ' . $this->age. ' years old <br>';
+    }
 }
+$first= new employee();
+$first->name = "John";
+$first->age = "24";
+$first->salary = "20,000";
+$first-> work();
+// print_r($first);
+
+
 
 
 ?>
