b54803e5ff6e0392ef12021ed32ff37e3943be4e
Huma Riaz <riazhuma2005@gmail.com>
2025-01-05 21:30:51 +0500

    Update overriding.php

--- overriding.php
+++ overriding.php
@@ -2,13 +2,25 @@
 
 class first{
     public $name = "Parent Class";
+
+    public function calc($a, $b){
+    return $a + $b;
+    }
 }
 
 class second extends first{
     public $name = "Child Class";
+
+    public function calc($a, $b){
+    return $a - $b;
+    }
 }
 
 $check = new second();
 echo $check->name;
+echo '<br>';
+
+$res = new second();
+echo $res->calc(15, 10);
 
 ?>
